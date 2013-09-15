ef3718e8413f52134871d7afd32e6c3ae5103f3b
Trambitskiy Yura <tr.yura@yahoo.com>
2013-09-15 21:52:43 +0400

    added gitignore, added node packages, added grunt.js

--- dist/centrizr.js
+++ dist/centrizr.js
@@ -1,29 +1,28 @@
-;(function($) {
+;
+(function ($) {
 
 
-$.fn.centrizr = function() {
+    $.fn.centrizr = function () {
 
-  var $container = $(this);
+        var $container = $(this);
 
-  $.container.addClass('centrizr-container');
-  $container.each(function() {
-      var $img = $(this).find('img');
-      var halfWidth = $img.width()/2;
-      var parentWidth = $container.width();
-      
-      if ($img.width()>parentWidth) {
-        $img.css('margin-left',-halfWidth + 'px');
-      } else {
-        $img.addClass('tall');
-        var halfHeight = $(this).height()/2;
-        $img.css('margin-top',-halfHeight + 'px');
-      }
-      $img.animate({'opacity': 1}, 200);
-    });
+        $.container.addClass('centrizr-container');
+        $container.each(function () {
+            var $img = $(this).find('img');
+            var halfWidth = $img.width() / 2;
+            var parentWidth = $container.width();
 
-  });
+            if ($img.width() > parentWidth) {
+                $img.css('margin-left', -halfWidth + 'px');
+            } else {
+                $img.addClass('tall');
+                var halfHeight = $(this).height() / 2;
+                $img.css('margin-top', -halfHeight + 'px');
+            }
+            $img.animate({'opacity': 1}, 200);
+        });
 
-}
+    }
 
 }(jQuery));
 
